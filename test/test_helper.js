a8f657b697b4454002d9400ac797138c57e5bf58
Tadeu Zagallo <tadeuzagallo@gmail.com>
2014-04-29 17:17:20 -0300

    mock localStorage to allow testing via cli

--- test/test_helper.js
+++ test/test_helper.js
@@ -13,4 +13,26 @@ global.catching = function (done, fn) {
   }
 };
 
+global.localStorage = (function () {
+  var cache = {};
+
+  return {
+    setItem: function (key, value) {
+      cache[key] = value;
+    },
+    getItem: function (key) {
+      return cache[key] || null;
+    },
+    removeItem: function (key) {
+      delete cache[key];
+    },
+    get length() {
+      return Object.keys(cache).length;
+    },
+    key: function (index) {
+      return Object.keys(cache)[index];
+    }
+  }
+})();
+
 chai.should();
